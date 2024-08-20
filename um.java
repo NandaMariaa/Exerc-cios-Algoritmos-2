52c4774e06286f359db1f281d89504ae09d14b1e
MariaNand4 <168575583+NandaMariaa@users.noreply.github.com>
2024-08-20 14:48:53 -0300

    oi

---
+++ um.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class um {
+
+public static void main(String[] args) throws Exception {
+       
+        double[] nota = new double[5];
+        double soma = 0.0, media;
+        int i ;
+        int TAM = 5;
+   
+        Scanner leitor = new Scanner(System.in);
+   
+        System.out.println( "Digite os valores das cinco notas:");
+            for (i = 0 ; i < TAM ; i ++ ){
+                    nota [i] = leitor.nextDouble();
+                    soma = soma + nota [i];
+                }
+                    media = soma/5 ;
+                    System.out.println("Média das notas: " + media ) ;
+           
+            }
+
+
+}
